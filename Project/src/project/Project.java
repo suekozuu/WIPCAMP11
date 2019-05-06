163fa2bde1a8aab7e582398343ff9cbe100c8121
ีunset <unset>
2019-05-06 16:33:15 +0700

    สร้างโปรเจกต์

---
+++ Project/src/project/Project.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author INT105
+ */
+public class Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int itemCount = 0;
+        int limit = 10;
+        
+        System.out.println(itemCount);
+    }
+    
+}
